d31151f71ec4d6c99e21f7803314926c7988c4f2
ikrizanic <ikrizanic75@gmail.com>
2020-12-27 17:50:46 +0100

    [2] + [3] Word embeddings with Spacy

---
+++ src/Postprocessing.py
@@ -0,0 +1,32 @@
+from nltk import WordNetLemmatizer
+from nltk.corpus import stopwords
+import spacy
+
+
+class Postprocessing:
+    """
+    Methods:
+        remove_stopwords(list): returns list of tokens without stopwords
+        lemmatize(list): returns lemmas of words from the tokens list
+        embed(list): returns vectors made with spacy
+        embed_all(list): returns vectors for all data
+    """
+
+    def __init__(self, MAX_NUMBER_OF_VECTORS=30):
+        self.stop_words = set(stopwords.words('english'))
+        self.lemmatizer = WordNetLemmatizer()
+        self.nlp = spacy.load("en_core_web_lg")
+        self.MAX_NUMBER_OF_VECTORS = MAX_NUMBER_OF_VECTORS
+
+    def remove_stopwords(self, tokens):
+        return [word for word in tokens if word not in self.stop_words]
+
+    def lemmatize(self, tokens):
+        return [self.lemmatizer.lemmatize(token) for token in tokens]
+
+    def embed(self, tokens):
+        return [self.nlp(token).vector for token in tokens][:self.MAX_NUMBER_OF_VECTORS]
+
+    def embed_all(self, data):
+        return [self.embed(tokens) for tokens in data]
+
--- src/Utils.py
+++ src/Utils.py
@@ -1,5 +1,12 @@
 import pandas as pd
+import pickle
 
 
-def load_dataset(path, encoding="ISO-8859-1", names=["label", "id", "date", "flag", "user", "text"]):
+def load_dataset(path, encoding="ISO-8859-1", names=None):
+    if names is None:
+        names = ["label", "id", "date", "flag", "user", "text"]
     return pd.read_csv(path, encoding=encoding, names=names)
+
+
+def save_embeddings(embeddings, path):
+    pickle.dump(embeddings, path)
