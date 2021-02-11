a5df605ff6d5236dd09f7c3d9a95725a08c43cc9
ikrizanic <ikrizanic75@gmail.com>
2021-02-11 19:25:53 +0100

    Changed main flask file structure

--- app.py
+++ app.py
@@ -1,30 +1,38 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_cors import CORS
 from pickle import load
-from src.api.Preprocessing import Preprocessing
-from src.api.Postprocessing import Postprocessing
-from src.api.Embedding import Embedding
 from src.api.PredictionPipeline import PredictionPipeline
 from tensorflow import keras
 
 app = Flask(__name__, template_folder='src/frontend/templates')
 
 
-prep = Preprocessing()
-post = Postprocessing()
-emb = Embedding()
-with open("data/vocab.pl", "rb") as f:
-    vocab = load(f)
-model = keras.models.load_model("./saved_models/")
-pp = PredictionPipeline(vocab, model)
-print("Init done.")
+class Pipeline:
+    def __init__(self):
+        with open("data/vocab.pl", "rb") as f:
+            vocab = load(f)
+        model = keras.models.load_model("./saved_models/")
+        self.pp = PredictionPipeline(vocab, model)
+
+    def predict(self, text):
+        return 1 - self.pp.get_prediction(text)[0][0]
 
 
-@app.route('/', methods=['POST', 'GET'])
+pipeline = None
+
+
+@app.route('/')
+def init():
+    global pipeline
+    pipeline = Pipeline()
+    return redirect(url_for('get_text'))
+
+
+@app.route('/home', methods=['POST', 'GET'])
 def get_text():
     if request.method == 'POST':
         text = request.form['text']
-        result = 1. - pp.get_prediction(text)[0][0]
+        result = pipeline.predict(text)
         return render_template('home.html', text=text, result=result)
     else:
         return render_template('home.html', text="", result="")
